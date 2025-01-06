903243fc2ebfc1e6e15c80595fde7485c84c11cc
rcaleb <calebmadara58@gmail.com>
2025-01-06 10:37:42 +0300

    feat: add user registration, login, and logout routes
    - Added `UserRegAndLogin` function to set up user-related routes:
      - Initialized `AuthController` with the provided database connection.
      - Registered the following routes:
        - `/check_login`: Checks if a user is logged in.
        - `/register`: Handles user registration.
        - `/login`: Handles user login and session creation.
        - `/logout`: Handles user logout and session deletion.
    - Integrated handlers from the `handlers` package for route functionality.

---
+++ BackEnd/routes/userRegAndLogin.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/Raymond9734/forum.git/BackEnd/controllers"
+	"github.com/Raymond9734/forum.git/BackEnd/handlers"
+)
+
+func UserRegAndLogin(db *sql.DB) {
+	AuthController := controllers.NewAuthController(db)
+
+	http.HandleFunc("/check_login", handlers.CheckLoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler(AuthController))
+	http.HandleFunc("/login", handlers.LoginHandler(AuthController))
+	http.HandleFunc("/logout", handlers.LogoutHandler)
+}
