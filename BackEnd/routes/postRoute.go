42dccbdbac2c64851cef98300c8d9ff2cad5354b
rcaleb <calebmadara58@gmail.com>
2025-01-08 15:11:36 +0300

    feat: Add PostRoutes for handling post-related routes
    - Introduced `PostRoutes` to register the `/createPost` endpoint.
    - Initialized `PostController` and passed it to `PostHandler` to handle post creation requests.

---
+++ BackEnd/routes/postRoute.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/Raymond9734/forum.git/BackEnd/controllers"
+	"github.com/Raymond9734/forum.git/BackEnd/handlers"
+)
+
+func PostRoutes(db *sql.DB) {
+	PostController := controllers.NewPostController(db)
+
+	http.HandleFunc("/createPost", handlers.PostHandler(PostController))
+}
