760e26c35cb6b8f7a265434da2fce87608e413a5
Vincent-Omondi <vincentomondi251@gmail.com>
2025-01-12 00:08:10 +0300

    feat: add route handler for uploaded images

--- BackEnd/routes/serveStaticFolder.go
+++ BackEnd/routes/serveStaticFolder.go
@@ -6,4 +6,5 @@ import (
 
 func ServeStaticFolder() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./FrontEnd/static"))))
+	http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 }
