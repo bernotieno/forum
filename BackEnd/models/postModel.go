2a38c303c13ffcea4904e39f8ccf7b9fcbcb7ca7
Vincent-Omondi <vincentomondi251@gmail.com>
2025-01-12 00:08:10 +0300

    feat: add ImageUrl field to Post struct

--- BackEnd/models/postModel.go
+++ BackEnd/models/postModel.go
@@ -18,7 +18,7 @@ type Post struct {
 	Dislikes  int
 	UserVote  sql.NullString // Can be "like", "dislike", or null
 	Content   string
-	ImageUrl  string
+	ImageUrl  sql.NullString
 	Timestamp time.Time
 }
 
