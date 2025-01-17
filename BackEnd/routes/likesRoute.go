44ba8b36c163aba920f1f94b18a263dc49aa4a82
rcaleb <calebmadara58@gmail.com>
2025-01-17 16:04:00 +0300

    feat: update likes routes to use user vote handler and add user votes retrieval

--- BackEnd/routes/likesRoute.go
+++ BackEnd/routes/likesRoute.go
@@ -18,11 +18,19 @@ func LikesRoutes(db *sql.DB) {
 	likesLimiter := middleware.NewRateLimiter(30, time.Minute) // 30 likes per minute
 
 	http.Handle("/likePost", middleware.ApplyMiddleware(
-		handlers.CreateLikeHandler(LikesController),
+		handlers.CreateUserVoteHandler(LikesController),
 		middleware.SetCSPHeaders,
 		middleware.AuthMiddleware,
 		middleware.CORSMiddleware,
 		likesLimiter.RateLimit,
+		middleware.VerifyCSRFMiddleware(db),
+	))
+
+	http.Handle("/getUserVotes", middleware.ApplyMiddleware(
+		handlers.GetUserVotesHandler(LikesController),
+		middleware.SetCSPHeaders,
+		middleware.AuthMiddleware,
+		middleware.CORSMiddleware,
 		middleware.VerifyCSRFMiddleware(db),
 	))
 }
