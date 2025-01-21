282698ccc6d7493b816f9b98d13a0f78caee8eaa
rcaleb <calebmadara58@gmail.com>
2025-01-21 12:35:26 +0300

    feat: Add error handling middleware to comment routes

--- BackEnd/routes/commenRoutes.go
+++ BackEnd/routes/commenRoutes.go
@@ -22,6 +22,7 @@ func CommentRoute(db *sql.DB) {
 		middleware.AuthMiddleware,
 		middleware.CORSMiddleware,
 		commentLimiter.RateLimit,
+		middleware.ErrorHandler,
 		middleware.VerifyCSRFMiddleware(db),
 	))
 
@@ -31,6 +32,7 @@ func CommentRoute(db *sql.DB) {
 		middleware.AuthMiddleware,
 		middleware.CORSMiddleware,
 		commentLimiter.RateLimit,
+		middleware.ErrorHandler,
 		middleware.VerifyCSRFMiddleware(db),
 	))
 
@@ -40,6 +42,7 @@ func CommentRoute(db *sql.DB) {
 		middleware.AuthMiddleware,
 		middleware.CORSMiddleware,
 		commentLimiter.RateLimit,
+		middleware.ErrorHandler,
 		middleware.VerifyCSRFMiddleware(db),
 	))
 }
