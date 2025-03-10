0f47f4aade9b9ee362561885f8ba18f84eca507d
Raymond9734 <rymadara97@gmail.com>
2025-03-10 22:43:03 +0300

    feat: extend post model with vote tracking and voted comments

--- BackEnd/models/postModel.go
+++ BackEnd/models/postModel.go
@@ -7,22 +7,31 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type VotedComment struct {
+	CommentID      int64
+	CommentContent string
+	UserVote       string
+}
+
 // Post represents a forum post
 type Post struct {
-	ID        int
-	IsAuthor  bool
-	Title     string
-	Author    string
-	UserID    int
-	Category  string
-	Likes     int
-	Dislikes  int
-	UserVote  sql.NullString 
-	Content   string
-	ImageUrl  sql.NullString
-	Timestamp time.Time
-	Comments  []Comment
-	CommentCount int
+	ID             int
+	IsAuthor       bool
+	Title          string
+	Author         string
+	UserID         int
+	Category       string
+	Likes          int
+	Dislikes       int
+	UserVote       sql.NullString
+	Content        string
+	ImageUrl       sql.NullString
+	Timestamp      time.Time
+	Comments       []Comment
+	CommentCount   int
+	VoteSource     string
+	VotedCommentID int64
+	VotedComments  []VotedComment
 }
 
 type PostRequest struct {
